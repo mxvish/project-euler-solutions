ce547a1527e428fe918c60afd8f8fdcae806a556
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-01 21:27:34 +0900

    add a less big-O complexity solution

--- problem3.py
+++ problem3.py
@@ -1,3 +1,4 @@
+#solution 1
 prime_numbers = [2]
 num = 600851475143
 
@@ -9,3 +10,17 @@ for x in range(3, num + 1):
             prime_numbers.append(x)
 
 print(prime_numbers[len(prime_numbers) - 1])
+
+#solution 2 (less big-O complexity)
+n = 600851475143
+div, maxFact = 2, 0
+
+while n != 0:
+    if n % div != 0:
+        div += 1
+    else:
+        maxFact = n
+        n = n / div
+        if n == 1:
+            print(int(maxFact))
+            break
