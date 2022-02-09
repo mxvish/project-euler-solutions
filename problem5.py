c9ce004962160737557edde3b4980b2982e8a86b
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-09 12:49:26 +0900

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,36 @@
+#solution1
+"""
+def gcf(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcf(b, a%b)
+
+def lcm(a, b):
+    d = gcf(a, b)
+    return a/d*b
+
+numbers = []
+key = 20
+#key = 10
+
+for x in range(1, key+1):
+    numbers.append(x)
+
+while len(numbers) > 1:
+    numbers.append(lcm(numbers[0], numbers[1]))
+    del numbers[:2]
+print(numbers)
+"""
+
+#solution2
+import numpy as np
+
+numbers = []
+key = 20
+#key = 10
+
+for x in range(1, key+1):
+    numbers.append(x)
+
+print(np.lcm.reduce(numbers))
