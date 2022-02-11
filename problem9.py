ac54b0a59aaedc0e732311c93c91e7a6786fb7ce
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-11 21:02:14 +0900

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -3,7 +3,7 @@ import math, sys
 total = 1000
 #total = 12
 
-for a in range(int(total/3)):
+for a in range(1, int(total/3)):
     for b in range(a+1, int(total/2)):
         for c in range(b+1, total):
             if a+b+c == total and math.pow(a,2) + math.pow(b,2) == math.pow(c,2):
