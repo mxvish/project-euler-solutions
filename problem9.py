52929d5c77008113ae0ecde3f845a20d45e68788
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-11 21:07:35 +0900

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -1,3 +1,5 @@
+#solution 1
+"""
 import math, sys
 
 total = 1000
@@ -9,3 +11,16 @@ for a in range(1, int(total/3)):
             if a+b+c == total and math.pow(a,2) + math.pow(b,2) == math.pow(c,2):
                 print(a, b, c, a*b*c)
                 sys.exit()
+"""
+#solution 2(less time complexity, 0.05s user 0.02s system 80% cpu 0.085 total)
+import sys
+
+total = 1000
+#total = 12
+
+for a in range(1, int(total/3)):
+    for b in range(a+1, int(total/2)):
+        c = total - a - b
+        if a*a + b*b == c*c:
+            print(a, b, c, a*b*c)
+            sys.exit()
