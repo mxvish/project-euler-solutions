7591503eab649843442372ba0fa45dfcf6e069a7
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-11 21:13:06 +0900

    added runtime length

--- problem6.py
+++ problem6.py
@@ -1,12 +1,11 @@
-import math
-
+#0.03s user 0.02s system 55% cpu 0.093 total
 a, b = 0, 0
 key = 100
 #key = 10
 
 for x in range(key):
-    a += math.pow(x+1, 2)
+    a += (x+1)*(x+1)
     b += x+1
 print(a)
-print(math.pow(b, 2))
-print(abs(a-math.pow(b, 2)))
+print(b*b)
+print(abs(a-b*b))
