1b66d212bf21928007699883dff74291ba3ef140
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-02-22 20:29:32 +0900

    Update problem10.py

--- problem10.py
+++ problem10.py
@@ -1,3 +1,5 @@
+#solution 1: takes longer than 1 min
+"""
 primes = [2]
 maxnum = 2000000
 #maxnum = 10
@@ -9,3 +11,18 @@ while key < maxnum-2:
         primes.append(key)
 
 print(sum(primes))
+"""
+
+#solution 2: 2.61s user 0.03s system 99% cpu 2.653 total
+def sum_primes(limit):
+    primes = [2]
+    for n in range(3, limit+1, 2):
+        for p in primes:
+            if n % p == 0: break   
+            if n < p*p:
+               primes.append(n)    
+               break
+        else: primes.append(n)     
+    return sum(primes)
+    
+print(sum_primes(2000000))
